f0e8e27ff41072d3c37d9c3e7995a56fe155fae5
Dmitry Gerashchenko <geraschenko.digital@gmail.com>
2025-07-27 21:12:57 +0300

    new pages: add telecom

---
+++ src/pages/Telecommunication/Telecommunication.jsx
@@ -0,0 +1,63 @@
+import SubPageDecor from '../../components/SubPageDecor/SubPageDecor';
+import './Telecommunication.scss';
+import HeadingHeroPageEl from "../../components/HeadingHeroPageEl/HeadingHeroPageEl.jsx";
+import BlockDividerText from "../../components/BlockDividerText/BlockDividerText.jsx";
+import SubPagePlayer from "../../components/SubPageDecor/SubPagePlayer.jsx";
+import BlockWithText from "../../components/BlockWithText/BlockWithText.jsx";
+export default () => {
+    const els = [
+        {
+            title: 'Core Telecom Infrastructure',
+            text: 'Fiber networks, towers, and data highways designed to support digital growth, latency-free connectivity, and regional integration.',
+            left: true
+        },
+        {
+            title: 'Dynamic Network Intelligence',
+            text: 'AI-driven systems optimizing bandwidth, managing network loads, and predicting disruptions across complex telecom grids.',
+            left: false
+        },
+        {
+            title: 'Scalable Edge & Satellite Systems',
+            text: 'Low-orbit satellite constellations and edge nodes expanding coverage, enabling real-time communication across remote zones.',
+            left: true
+        },
+        {
+            title: 'Sovereign Digital Access Agreements',
+            text: 'Cross-sector alliances delivering national connectivity goals, infrastructure security, and inclusive digital development through long-term public-private models.',
+            left: false
+        }
+    ]
+
+    return (
+      <>
+          <SubPageDecor
+            frameCount={97}
+            basePath={'/sectorsSequences/TelecomLarge/'}
+          />
+          <HeadingHeroPageEl title={'Telecommunication'}/>
+          <BlockDividerText
+            className={'Telecom'}
+            center
+            title={'Expanding Critical Telecommunication Networks'}
+            text={'Investing in fiber, satellite, and 5G infrastructure to enable seamless connectivity, digital inclusion, and national-scale data resilience.'}
+          />
+          <div className='Telecom container'>
+              <SubPagePlayer
+                forwardSrc="/video/Telecom.mp4"
+                reverseSrc="/video/reverse_Telecom.mp4"
+              />
+              <div className='Telecom_content'>
+                  <div className='Telecom_info mt_xl'>
+                      {
+                          els.map((el, index) => (
+                            <BlockWithText className={'Telecom'} title={el.title} text={el.text} text2={el.text2} left={el.left}
+                                           index={index + 1} key={`BlockWithLines_${index}`}/>
+                          ))
+                      }
+                  </div>
+
+              </div>
+          </div>
+      </>
+    )
+}

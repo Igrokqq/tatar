48acfb8bf447e730ef65539e673f384d7eb4db12
Dmitry Gerashchenko <geraschenko.digital@gmail.com>
2025-07-28 22:11:59 +0300

    improvements: remove BlockDivider text animation

--- src/components/BlockDividerText/BlockDividerText.jsx
+++ src/components/BlockDividerText/BlockDividerText.jsx
@@ -8,32 +8,9 @@ import { themeStore } from "../../stores/themeStore.js";
 gsap.registerPlugin(ScrollTrigger)
 
 export default ({ title, text, text2, left, center, index, className }) => {
-    const blockRef = useRef(null);
 
-    useEffect(() => {
-        const direction = index % 2 === 0 ? -200 : 200;
-
-        gsap.fromTo(
-        blockRef.current,
-        {
-            x: direction,
-            opacity: 0,
-        },
-        {
-            x: 0,
-            opacity: 1,
-            duration: 1,
-            ease: 'power3.out',
-            scrollTrigger: {
-            trigger: blockRef.current,
-            start: 'top 70%',
-            },
-        }
-        );
-    }, []);
-    
     return (
-    <div className={`BlockDividerText ${className} ${!themeStore.isLight && 'BlockDividerText_light'}  BlockDividerText ${left && 'BlockDividerText_left'} ${left && 'BlockDividerText_center'}`}  ref={blockRef}>
+    <div className={`BlockDividerText ${className} ${!themeStore.isLight && 'BlockDividerText_light'}  BlockDividerText ${left && 'BlockDividerText_left'} ${left && 'BlockDividerText_center'}`}>
         <div className={`BlockDividerText_content ${left && 'BlockDividerText_content_left'} ${center && 'BlockDividerText_content_center'}`}>
             <div className='BlockDividerText_content_title'>
                 {title}
